b0a65245691b7ac06d25455e55c42d65d252fdcc
Jacqueline Chapman <jacquelinechapman@me.com>
2015-04-26 15:05:43 -0400

    Add form helpers and create action

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,9 +8,30 @@ class PinsController < ApplicationController
     @pin = Pin.find(params[:id])
   end
 
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.create(pin_params)
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :new
+    end
+  end
+
   def show_by_name
   	@pin = Pin.find_by_slug(params[:slug])
-  	render :show 
+  	render :show
   end
-  
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+  end
+
 end
